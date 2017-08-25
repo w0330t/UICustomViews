bf91eb14946af5b5199c4cd4e906f41841528abd
Blueness <w0330t@gmail.com>
2017-08-25 11:04:39 +0800

    Build Title complete

--- app/src/main/java/net/blueness/uicustomviews/MainActivity.kt
+++ app/src/main/java/net/blueness/uicustomviews/MainActivity.kt
@@ -2,11 +2,16 @@ package net.blueness.uicustomviews
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.ActionBar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val actionBar = supportActionBar as ActionBar
+        if (actionBar != null){
+            actionBar.hide()
+        }
     }
 }
